0c672a93a541908fbd4b8b1b73a2036fb3ff2e05
Kristin Chen <kristin.chen@blend360.com>
2022-04-27 11:37:26 -0400

    Add comments

--- data_cleaning.sql
+++ data_cleaning.sql
@@ -1,7 +1,7 @@
 -- 1. Clean job capability
 -- select distinct candidateId, jobId, jobCapability from ApplicationEvents_Merged where jobCapability is NULL
 
--- 2. remove invalid events (table: ValidEvents)
+-- 2. remove invalid events (table #1: ValidEvents; code: step1)
 -- select eventType, applicationStatus, applicationSubStatus from ApplicationEvents_Merged group by eventType, applicationStatus, applicationSubStatus order by applicationStatus
 
 -- 3. remove prior unclosure history before re-submitting
@@ -11,9 +11,16 @@
 -- bad one:
 -- select * from ValidEvents where candidateid = '02d0da8f-5018-48a0-9fd7-41acd000afec'; 
 
--- 4. assign `joinId` to identify the # of join for resubmitted applicants (table: CleanedValidEvents)
+-- 4. assign `joinId` to identify the # of join for resubmitted applicants (table 2: CleanedValidEvents; code: step2)
 select * from CleanedValidEvents where candidateid = '0067828a-1eb5-4a3e-9afa-330b72f30bb2' order by eventDate; 
 select * from CleanedValidEvents where candidateid = '02d0da8f-5018-48a0-9fd7-41acd000afec' order by eventDate; 
 
 -- 5. remove records with same status but different eventId and eventDate (haven't done on my end)
 select * from CleanedValidEvents  where candidateid = '001baac4-baec-40ca-aac6-c9c8cecd477c' order by eventDate; 
+
+-- 6. categorize applicants (code: step3)
+-- join
+-- in funnel
+-- active in funnel
+-- inactive in funnel
+-- out
