b384910aa231a435d59e325e8d37a8860c10add3
Kristin Chen <kristin.chen@blend360.com>
2022-04-28 12:08:04 -0400

    Clean comments

--- data_cleaning.sql
+++ data_cleaning.sql
@@ -1,26 +1,19 @@
 -- 1. Clean job capability
--- select distinct candidateId, jobId, jobCapability from ApplicationEvents_Merged where jobCapability is NULL
+-- select * from ApplicationEvents_Merged where jobCapability is NULL 
 
 -- 2. remove invalid events (table #1: ValidEvents; code: step1)
 -- select eventType, applicationStatus, applicationSubStatus from ApplicationEvents_Merged group by eventType, applicationStatus, applicationSubStatus order by applicationStatus
 
 -- 3. remove prior unclosure history before re-submitting
 -- good one: 
--- select * from ValidEvents where candidateid = '0067828a-1eb5-4a3e-9afa-330b72f30bb2' order by eventDate; 
+-- select * from ValidEvents where candidateid = '0067828a-1eb5-4a3e-9afa-330b72f30bb2' and jobid = 'ace13ce4-eb17-4094-8576-39c821029a90' order by eventDate; 
 
 -- bad one:
--- select * from ValidEvents where candidateid = '02d0da8f-5018-48a0-9fd7-41acd000afec'; 
+-- select * from ValidEvents where candidateid = '02d0da8f-5018-48a0-9fd7-41acd000afec' order by eventdate; 
 
--- 4. assign `joinId` to identify the # of join for resubmitted applicants (table 2: CleanedValidEvents; code: step2)
+-- 4. assign `joinId` to identify the # of join for resubmitted applicants (table 2: CleanedValidEvents; code: step2) --handle by the analysis team
 select * from CleanedValidEvents where candidateid = '0067828a-1eb5-4a3e-9afa-330b72f30bb2' order by eventDate; 
 select * from CleanedValidEvents where candidateid = '02d0da8f-5018-48a0-9fd7-41acd000afec' order by eventDate; 
 
--- 5. remove records with same status but different eventId and eventDate (haven't done on my end)
-select * from CleanedValidEvents  where candidateid = '001baac4-baec-40ca-aac6-c9c8cecd477c' order by eventDate; 
-
--- 6. categorize applicants (code: step3)
--- join
--- in funnel
--- active in funnel
--- inactive in funnel
--- out
+-- 5. remove records with same status but different eventId and eventDate 
+select * from ApplicationEvents_Merged  where candidateid = '001baac4-baec-40ca-aac6-c9c8cecd477c' order by eventDate; 
