f5c832d699855148a439496a0e017c19fb5d9d11
Kristin Chen <kristin.chen@blend360.com>
2022-05-09 16:46:55 -0400

    Update cleaning

--- step1_clean_status_and_remove_invalid_events.sql
+++ step1_clean_status_and_remove_invalid_events.sql
@@ -1,8 +1,4 @@
--- each applicant is identified by `candidateId` and `jobId`
--- each applicant join/rejoin once; and exit once
-
 -- Clean job capability
--- select distinct candidateId, jobId, jobCapability from ApplicationEvents_Merged where jobCapability is NULL
 with CleanedJobCapabilityTable as 
 (
 select distinct tot.eventId, tot.candidateId, tot.jobId, tot.jobLevel, c.cleanedJobCapability jobCapability, tot.jobLocation, tot.eventDate, tot.eventType, tot.applicationStatus, tot.applicationSubStatus from
@@ -17,12 +13,7 @@ right join
 ApplicationEvents_Merged tot
 on
 tot.candidateId = c.candidateId and tot.jobid = c.jobid 
-), 
--- select * from CleanedJobCapabilityTable where candidateid = '001baac4-baec-40ca-aac6-c9c8cecd477c' order by eventDate
-
-
--- Remove invalid events 
--- good example: select * from CleanedJobCapabilityTable where candidateid = '001baac4-baec-40ca-aac6-c9c8cecd477c' order by eventDate; 
+),
 
 CleanedStatusTable as 
 (
@@ -31,13 +22,13 @@ select distinct eventId, candidateId, jobId, jobLevel, jobCapability, jobLocatio
     select
     *,
     case when eventType like '%application created%' and applicationStatus like '%lead%' then 'NEW' 
-        when eventType like '%application re-submitted%' and applicationStatus like '%lead%' then 'NEW' 
+         when eventType like '%application re-submitted%' and applicationStatus like '%lead%' then 'NEW' 
+         when applicationStatus like '%interview%' then 'IN_REVIEW' 
+
     else applicationStatus end cleanedApplicationStatus,
-    case when applicationStatus like '%hired%' then 'Application Status Updated'
-        when applicationStatus like '%rejected%' and eventType not like '%application created%' then 'Application Status Updated'
-        when applicationStatus like '%offered%' and applicationSubStatus like '%offer pending%' then 'Application Status Updated'
-        when applicationStatus like '%transferred%' then 'Application Status Updated'
-        when applicationStatus like '%withdrawn%' then 'Application Status Updated'
+    
+    case when eventType like '%application fields updated%' or eventType like '%application source updated%' or eventType like '%application screening answers updated%' then 'Application Status Updated'
+
     else eventType end cleanedEventType
     from CleanedJobCapabilityTable
 ) a 
@@ -48,9 +39,9 @@ validCases as (
     select *,
     case when applicationStatus like'%in_review%' and applicationSubStatus IS NULL then 1
     when applicationStatus like '%interview%' and applicationSubStatus IS NULL then 1
-    when applicationStatus like '%application fields updated%' and applicationSubStatus like '%LEAD%' then 1
-    when applicationStatus like '%NEW%' and (eventType not like '%application created%' and eventType not like '%application re-submitted%') then 1
-    when applicationStatus like '%LEAD%' and (eventType not like '%application created%' and eventType not like '%application re-submitted%') then 1
+    when eventType like '%application status updated%' and applicationStatus like '%LEAD%' then 1
+    when eventType like '%application status updated%' and applicationStatus like '%NEW%' then 1
+
     else 0 end invalidFlag from 
     (
         select distinct eventId, candidateId, jobId, jobLevel, jobCapability, jobLocation, eventDate, cleanedApplicationStatus applicationStatus, cleanedEventType eventType, applicationSubStatus from CleanedStatusTable
@@ -59,9 +50,6 @@ validCases as (
     where invalidFlag = 0
 )
 
--- Remove dupuplicate status records caused by different eventDate and eventType; 
--- good example: select * from validCases where candidateid = '001baac4-baec-40ca-aac6-c9c8cecd477c' order by eventDate; 
--- select * from validCases where candidateid = '001baac4-baec-40ca-aac6-c9c8cecd477c' order by eventDate; 
-
 -- IF EXISTS(SELECT * FROM dbo.ValidEvents) DROP TABLE dbo.ValidEvents
 SELECT * INTO dbo.ValidEvents FROM validCases;
+
