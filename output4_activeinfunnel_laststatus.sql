0b83d8adb0e35dfb2e4641ae7e593823c01778b1
Kristin Chen <kristin.chen@blend360.com>
2022-06-09 23:56:03 -0400

    Add last funnel reporting code

--- output4_activeinfunnel_laststatus.sql
+++ output4_activeinfunnel_laststatus.sql
@@ -1,17 +1,7 @@
-With ActiveInFunnelLastStatusTable AS (
-SELECT jobCapability, jobLocation, experiencedFlag, eventType, applicationStatus, applicationSubStatus, count(distinct uniqueId) as numApplicants from
+-- report 1 -----------------------------------
+SELECT jobCapability, jobLocation, experiencedFlag, analyticalStatus, count(distinct uniqueId) as numApplicants from
 (
-SELECT c.*, 
-    case when c.jobLevel not like '%all-star%' and c.jobLevel not like '%intern%' then 1 
-         when c.jobLevel like '%all-star%' then 0 else NULL end as experiencedFlag
+SELECT c.*
 FROM ActiveInFunnelData c where Step = maxStep
 ) b
-group by jobCapability, jobLocation, experiencedFlag, eventType, applicationStatus, applicationSubStatus)
-
--- select count(distinct uniqueId) from ActiveInFunnelData; --520
--- select sum(numApplicants) from ActiveInFunnelLastStatusTable; --520
-
--- IF EXISTS(SELECT * FROM  dbo.ActiveInFunnelLastStatusTable) DROP TABLE dbo.ActiveInFunnelLastStatusTable;
-SELECT * INTO dbo.ActiveInFunnelLastStatusTable FROM ActiveInFunnelLastStatusTable
-
-select * from ActiveInFunnelLastStatusTable
+group by jobCapability, jobLocation, experiencedFlag, analyticalStatus
